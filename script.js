b9f70f0b9a714c7f171fb81461e0211d9a71535f
Devon Kelly <devon@Devons-MacBook-Pro.local>
2015-06-11 10:06:06 -0500

    Split data/tabs into System & Regions

--- script.js
+++ script.js
@@ -10,33 +10,35 @@ $(function () {
             context: document.body
         }).done(function (data) {
             var currentRegion, html;
-//            document.getElementById("dynamic").innerHTML = '';
+            document.getElementById("sys").innerHTML = '<h2><p>System Metrics</p></h2>';
+            document.getElementById("reg1").innerHTML = '<h2><p>Region 1</p></h2>';
+            document.getElementById("reg2").innerHTML = '<h2><p>Region 2</p></h2>';
+            document.getElementById("reg3").innerHTML = '<h2><p>Region 3</p></h2>';
+            document.getElementById("reg4").innerHTML = '<h2><p>Region 4</p></h2>';
+            document.getElementById("reg5").innerHTML = '<h2><p>Region 5</p></h2>';
+            document.getElementById("reg6").innerHTML = '<h2><p>Region 6</p></h2>';
             for(i=0; i<=data.length-1; i++) {
                 console.log('Region', data[i].region);
-                if(currentRegion != data[i].region) {
-                    if(i != 0) {
-                        html += "</table>";
-                    }
-                    html +="<p><h2>Region " + data[i].region+"</h2>";
-                    html+="<table class='table'>";
+                document.getElementById("sys").innerHTML += '<h3><p>Region ' + data[i].region + ' - ' + data[i].regionData[0].station + '  - ' + data[i].regionData[0].lastUpdDt + '</p><h3>';
+                switch (data[i].region)
+                {
+                    case "1": document.getElementById("reg1").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    break;
+                    case "2": document.getElementById("reg2").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    break;
+                    case "3": document.getElementById("reg3").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    break;
+                    case "4": document.getElementById("reg4").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    break;
+                    case "5": document.getElementById("reg5").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    break;
+                    case "6": document.getElementById("reg6").innerHTML += '<h3><p> ' + data[i].regionData[0].station + ' - ' + data[i].regionData[0].lastUpdDt + '</p></h3>';
+                    default: console.log('Bad Region in data');
+                    break;
                 }
-                data[i].regionData.forEach(function(region, idx) {
-                    html+="<tr><td>Station:"+region.station+"</td><td>"+region.lastUpdDt+"</td></tr>";
-                    console.log(idx, region.station, region.lastUpdDt);
-                });
-                
+                console.log('Region: ' + data[i].region + ' Station: ' + data[i].regionData[0].station + ' LUD: ' + data[i].regionData[0].lastUpdDt);
                 currentRegion= data[i].region;
-            }
-            html += "</table>";
-            document.getElementById("dynamic").innerHTML = html;
-//            $('#dynamic').append(html);
-        });
+        };
     }
-    
-    function rows(k, region, record) {
-        return '<tr>' +
-            '<td class="col-md-2"><h4>' + k + ':*' + region + '* ' + record.station + '</h4></td>' +
-            '<td id="station"><h4>' + record.lastUpdDt + '</h4></td>' +
-            '</tr>';
-    }
+)};
 });
